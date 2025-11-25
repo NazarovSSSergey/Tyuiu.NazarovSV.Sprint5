de29ca30e62b0edf3070336e84e9ae12d0e21f55
Сергей Назаров <Tyuiu.NazarovSV@outlook.com>
2025-11-25 15:33:12 +0500

    Task0

--- Tyuiu.NazarovSV.Sprint5.Task0.V8.Lib/DataService.cs
+++ Tyuiu.NazarovSV.Sprint5.Task0.V8.Lib/DataService.cs
@@ -5,7 +5,7 @@ namespace Tyuiu.NazarovSV.Sprint5.Task0.V8.Lib
     {
         public string SaveToFileTextData(int x)
         {
-            string path = $@"{Directory.GetCurrentDirectory()}\OutPutFileTask0.txt";
+            string path = $@"{Path.GetTempPath()}\OutPutFileTask0.txt";
             double y = (Math.Pow(x, 3) - 1) / (4 * Math.Pow(x, 2));
             y = Math.Round(y, 3);
             File.WriteAllText(path, Convert.ToString(y));
