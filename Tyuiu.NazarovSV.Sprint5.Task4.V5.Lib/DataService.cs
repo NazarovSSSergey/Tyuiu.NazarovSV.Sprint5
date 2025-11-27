499e6623d4e0507677cfb9cd72c2ed003e57f886
Сергей Назаров <Tyuiu.NazarovSV@outlook.com>
2025-11-27 14:55:05 +0500

    task4

--- Tyuiu.NazarovSV.Sprint5.Task4.V5.Lib/DataService.cs
+++ Tyuiu.NazarovSV.Sprint5.Task4.V5.Lib/DataService.cs
@@ -1,4 +1,5 @@
-﻿using tyuiu.cources.programming.interfaces.Sprint5;
+﻿using System.Globalization;
+using tyuiu.cources.programming.interfaces.Sprint5;
 namespace Tyuiu.NazarovSV.Sprint5.Task4.V5.Lib
 {
     public class DataService : ISprint5Task4V5
@@ -6,9 +7,18 @@ namespace Tyuiu.NazarovSV.Sprint5.Task4.V5.Lib
         public double LoadFromDataFile(string path)
         {
             string strX = File.ReadAllText(path);
-            double y = 4.26 * Convert.ToDouble(strX) / Math.Sin(Convert.ToDouble(strX));
-            y = Math.Round(y, 3);
-            return y;
+            double x;
+
+            if (Double.TryParse(strX, NumberStyles.Any, CultureInfo.InvariantCulture, out x))
+            {
+                double y = 4.26 * x / Math.Sin(x);
+                y = Math.Round(y, 3);
+                return y;
+            }
+            else
+            {
+                throw new FormatException($"The input string '{strX}' was not in a correct format.");
+            }
         }
     }
 }
