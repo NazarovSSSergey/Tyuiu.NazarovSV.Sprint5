0dfcd3af0e472db96e7710e6a2df9326b31d2bb3
Сергей Назаров <Tyuiu.NazarovSV@outlook.com>
2025-11-27 13:42:46 +0500

    task3

---
+++ Tyuiu.NazarovSV.Sprint5.Task3.V29.Lib/DataService.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+using tyuiu.cources.programming.interfaces.Sprint5;
+using System.IO;
+namespace Tyuiu.NazarovSV.Sprint5.Task3.V29.Lib
+{
+    public class DataService : ISprint5Task3V29
+    {
+        public string SaveToFileTextData(int x)
+        {
+            string path = $@"{Path.GetTempPath()}\OutPutFileTask3.bin";
+            double y = Math.Pow(x, 3) + 2 * Math.Pow(x, 2) + 5 * x + 4;
+            y = Math.Round(y, 3);
+            using (BinaryWriter writer = new BinaryWriter(File.Open(path, FileMode.OpenOrCreate), Encoding.UTF8))
+            {
+                writer.Write(BitConverter.GetBytes(y));
+            }
+
+            return path;
+        }
+    }
+}
---
+++ Tyuiu.NazarovSV.Sprint5.Task3.V29/Program.cs
@@ -0,0 +1,23 @@
+﻿using Tyuiu.NazarovSV.Sprint5.Task3.V29.Lib;
+namespace Tyuiu.NazarovSV.Sprint5.Task3.V29
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            int x = 3;
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                       *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine(" x = " + x);
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                             *");
+            Console.WriteLine("**************************************************************************");
+            string res = ds.SaveToFileTextData(x);
+            Console.WriteLine("Файл: " + res);
+            Console.WriteLine("Создан! ");
+            Console.ReadKey();
+        }
+    }
+}
