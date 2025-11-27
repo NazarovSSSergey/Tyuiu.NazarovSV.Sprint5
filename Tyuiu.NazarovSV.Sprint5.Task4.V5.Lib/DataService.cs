c167faf36778ea375c475ce9860994459b5adf98
Сергей Назаров <Tyuiu.NazarovSV@outlook.com>
2025-11-27 14:50:54 +0500

    task4

---
+++ Tyuiu.NazarovSV.Sprint5.Task4.V5.Lib/DataService.cs
@@ -0,0 +1,14 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.NazarovSV.Sprint5.Task4.V5.Lib
+{
+    public class DataService : ISprint5Task4V5
+    {
+        public double LoadFromDataFile(string path)
+        {
+            string strX = File.ReadAllText(path);
+            double y = 4.26 * Convert.ToDouble(strX) / Math.Sin(Convert.ToDouble(strX));
+            y = Math.Round(y, 3);
+            return y;
+        }
+    }
+}
---
+++ Tyuiu.NazarovSV.Sprint5.Task4.V5/Program.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.NazarovSV.Sprint5.Task4.V5.Lib;
+namespace Tyuiu.NazarovSV.Sprint5.Task4.V5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                       *");
+            Console.WriteLine("**************************************************************************");
+            string path = $@"{Path.GetTempPath()}\InPutDataFileTask4V0.txt";
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                             *");
+            Console.WriteLine("**************************************************************************");
+            double res = ds.LoadFromDataFile(path);
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}
