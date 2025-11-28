09a7a77f6ad087ef1f6672ee1b3ffa6e55543678
Сергей Назаров <Tyuiu.NazarovSV@outlook.com>
2025-11-28 18:04:04 +0500

    Task7

--- Tyuiu.NazarovSV.Sprint5.Task7.V22/Program.cs
+++ Tyuiu.NazarovSV.Sprint5.Task7.V22/Program.cs
@@ -5,7 +5,21 @@ namespace Tyuiu.NazarovSV.Sprint5.Task7.V22
     {
         static void Main(string[] args)
         {
-            
+            DataService ds = new DataService();
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                       *");
+            Console.WriteLine("**************************************************************************");
+            string path = $@"{Path.GetTempPath()}\InPutDataFileTask7V22.txt";
+            string path1 = $@"{Path.GetTempPath()}\OutPutDataFileTask7V22.txt";
+            Console.WriteLine("Данные находятся в файле: " + path);
+            Console.WriteLine();
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                             *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("Находится в файле: ");
+            path1 = ds.LoadDataAndSave(path);
+            Console.WriteLine(path1);
+            Console.ReadKey();
         }
     }
 }
