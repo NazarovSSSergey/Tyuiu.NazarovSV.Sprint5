d561c0dfa792fecd0d10e6c84efb3b7150bb215c
Сергей Назаров <Tyuiu.NazarovSV@outlook.com>
2025-11-28 16:41:07 +0500

    Task7

---
+++ Tyuiu.NazarovSV.Sprint5.Task7.V29.Lib/DataService.cs
@@ -0,0 +1,38 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.NazarovSV.Sprint5.Task7.V29.Lib
+{
+    public class DataService : ISprint5Task7V29
+    {
+        public string LoadDataAndSave(string path)
+        {
+            string PSF = $@"{Path.GetTempPath()}\OutPutDataFileTask7V29.txt";
+
+            FileInfo fileInfo = new FileInfo(PSF);
+            bool fileExists = fileInfo.Exists;
+            if (fileExists)
+            {
+                File.Delete(PSF);
+            }
+            string strLine = "";
+            using (StreamReader reader = new StreamReader(PSF))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    for (int i = 0; i < line.Length; i++)
+                    {
+                        string p = Convert.ToString(Math.Abs(line[i]));
+                        if ( p.Length != 1)
+                        {
+                            strLine = strLine + p;
+                        }
+                    }
+
+                    File.AppendAllText(PSF, strLine + Environment.NewLine);
+                    strLine = "";
+                }
+            }
+            return PSF;
+        }
+    }
+}
---
+++ Tyuiu.NazarovSV.Sprint5.Task7.V29.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.NazarovSV.Sprint5.Task7.V29.Lib;
+namespace Tyuiu.NazarovSV.Sprint5.Task7.V29.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = Path.GetTempFileName();
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool next = true;
+            Assert.AreEqual(next, fileExists);
+        }
+    }
+}
